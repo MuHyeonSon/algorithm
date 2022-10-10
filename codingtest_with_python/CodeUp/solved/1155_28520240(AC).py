f597f788ba5de41273441fe7d76c12eeba8bb518
Muhyeon Son <smuhyeon84@gmail.com>
2022-10-10 20:41:08 +0900

    📰CodeUp 알고리즘문제 (1151~1173)
    "이것이취업을위한코딩테스트다" 학습 순서 2단계
    CodeUp 문제 1151~1173

---
+++ codingtest_with_python/CodeUp/solved/1155_28520240(AC).py
@@ -0,0 +1,13 @@
+﻿"""
+인학이는 숫자 7을 좋아한다.
+
+어떤 정수가 입력되면 그 수가 7의 배수인지 확인하시오.
+7의 배수일 경우 multiple를 출력하고, 7의 배수가 아니면 not multiple을 출력하시오.
+"""
+
+n = int(input())
+
+if n % 7 == 0:
+    print("multiple")
+else:
+    print("not multiple")
---
+++ codingtest_with_python/CodeUp/solved/1172_28521636(AC).py
@@ -0,0 +1,16 @@
+﻿"""
+세 정수가 입력된다.
+
+출력
+낮은 숫자 부터 출력한다.
+
+입력 예시   
+8 7 6
+
+출력 예시
+6 7 8
+"""
+a = list(map(int,input().split()))
+a.sort()
+for x in a:
+    print(x, end = ' ')
