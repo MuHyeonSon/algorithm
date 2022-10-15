79dd0624988ce64169088e7cd889c0d770d580cd
Muhyeon Son <smuhyeon84@gmail.com>
2022-10-15 20:04:45 +0900

    📰CodeUp 알고리즘문제 (1351~1358, ... ,1361)
    "이것이취업을위한코딩테스트다" 학습 순서 2단계
    CodeUp 문제  1351~1358, ... ,1361

---
+++ 1354_28598466(AC).py
@@ -0,0 +1,30 @@
+﻿"""
+길이 n이 입력되면 역삼각형을 출력한다.
+
+예)
+
+n이 5이면
+
+*****
+****
+***
+**
+*
+입력
+길이 n이 입력된다.
+
+출력
+역삼각형을 출력한다.
+"""
+n = int(input())
+for i in range(n,0,-1):
+    print('*'*i)
+    
+# 중첩반복문 이용하면
+"""
+n = int(input())
+for i in range(n,0,-1):
+    for _ in range(i):
+        print('*', end = '')
+    print()
+"""
