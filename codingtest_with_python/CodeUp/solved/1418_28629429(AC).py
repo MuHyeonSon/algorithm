4a77ec8daa135efacce2a1bb5e90c3adfba903c8
Muhyeon Son <smuhyeon84@gmail.com>
2022-10-18 12:23:28 +0900

    📰CodeUp 알고리즘문제 (1406, 1408, 1414, ... , 2721)
    "이것이취업을위한코딩테스트다" 학습 순서 2단계
    CodeUp 문제  1406, 1408, 1414, 1418, 1419, 1733, 1734, 1754, 1990, 2721

---
+++ codingtest_with_python/CodeUp/solved/1418_28629429(AC).py
@@ -0,0 +1,25 @@
+﻿"""
+어떤 문자열이 있을 때 문자 t의 위치를 모두 찾아 출력하시오.
+
+입력
+공백이 없는 문자열이 한 줄 입력된다.(10글자 이하)
+
+출력
+소문자 t의 위치를 공백으로 분리하여 모두 출력하시오.
+
+입력 예시   
+test
+
+출력 예시
+1 4 
+"""
+
+input_string = input() # 문자열 입력받음
+input_list = [x for x in input_string] # 입력받은 문자열에서 문자 하나씩 원소로 한 리스트 생성
+
+for i in range(len(input_list)):
+    if input_list[i] == 't': # i 번째 문자가 't'라면
+        print(str(i+1), end = ' ') # t의 위치 출력
+
+
+
---
+++ codingtest_with_python/CodeUp/solved/1419_28629539(AC).py
@@ -0,0 +1,21 @@
+﻿"""
+영어 문장이 입력된다.
+
+그 문장에서 love가 몇 번 나오는지 출력하시오.
+
+입력
+영어 한 문장이 입력된다.(공백 있음, 최대 글자수 100)
+
+출력
+소문자 love가 몇 번 나오는지 출력한다.
+
+입력 예시   
+love lovely
+
+출력 예시
+2
+"""
+
+input_string = input()
+
+print(input_string.count('love'))
