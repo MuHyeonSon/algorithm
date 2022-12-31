bb810940300c510b32c51cc0e12618f4e5c7b38b
Muhyeon Son <smuhyeon84@gmail.com>
2023-01-01 00:42:26 +0900

    Update 개선된_다익스트라_알고리즘.py

--- codingtest_with_python/최단경로/개선된_다익스트라_알고리즘.py
+++ codingtest_with_python/최단경로/개선된_다익스트라_알고리즘.py
@@ -7,39 +7,43 @@
 # 나의 풀이 (Product Code)
 
 
-
     
 # 교재 풀이
 
 import heapq
 import sys
-input = sys.stdin.readline
+from heapq import heappop
+from heapq import heappush
+from sys import stdin
+
+#input = sys.stdin.readline
 INF = int(1e9) # 무한을 의미하는 값 (10억)
 
 # 노드의 개수와 간선의 개수 입력받기
-n, m = map(int, input().split())
+n, m = map(int,input().split())
 # 시작 노드 번호를 입력받기
 start = int(input())
 # 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
-graph = [[] for i in range(n+1)] ######### 2ㅏㅊ원 리스트 초기화 방식 까먹지 말 것
+graph = [[] for i in range(n+1)] ######### 2차원 리스트 초기화 방식 까먹지 말 것
 # 최단 거리 테이블을 모두 무한으로 초기화
 distance = [INF]*(n+1)
 
 # 모든 간선 정보를 입력받기
 for _ in range(m):
-  a, b, c = map(int,input().split()) # a노드에서 b노드까지 가는 비용이 c만큼 듬
+  a, b, c = map(int,input().split()) 
+  # a노드에서 b노드까지 가는 비용이 c만큼 듬
   graph[a].append((b,c))
 
 def dijkstra(start):
   q = []
   # 시작 노드로 가기 위한 최단 경로는 0으로 설정, 큐에 삽입
-  heapq,heappush(q,(start,0))
+  heapq,heappush(q,(0, start)) # 거리, 노드 (파이썬의 q라이브러리는 원소로 튜플 입력 넣으면 튜플의 첫 번째 원소를 기준으로 우선순위 큐를 구성하기 때문)
   distance[start] = 0
   while q: # q가 비어있지 않다면
     # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
     dist, now = heapq.heappop(q)
     # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
-    if graph[now] < dist:
+    if distance[now] < dist:
       continue
     # 현재 노드와 연결되어 있는 다른 노드들을 확인
     for i in graph[now]:
@@ -47,13 +51,13 @@ def dijkstra(start):
       # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
       if cost < distance[i[0]]:
         distance[i[0]] = cost
-        heapq.heappush(q,(cost,i[0]))
+        heapq.heappush(q, (cost, i[0]))
 
 # 다익스트라 알고리즘 수행
 dijkstra(start)
 
 # 모든 노드로 가기 위한 최단 거리를 출력
-for i in range(1, n+1):
+for i in range(1, n + 1):
   # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
   if distance[i] == INF:
     print("INFINITY")
